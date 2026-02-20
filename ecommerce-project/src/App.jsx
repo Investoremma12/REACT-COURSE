03df947f55e1222ff05b7bc6599c8139c82d4986
emmanuel <onyibeemmanuel621@gmail.com>
2026-02-20 13:09:39 +0100

    move orders page to react

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router';
 import './App.css';
 import { HomePage } from './pages/HomePage';
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrderPage } from './pages/OrderPage';
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="checkout" element={<CheckoutPage />} />
+				<Route path="orders" element={<OrderPage />} />
 			</Routes>
 		</>
 	);
