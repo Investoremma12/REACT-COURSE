ec84e44ddbfb554df412085683d497e9541ff415
emmanuel <onyibeemmanuel621@gmail.com>
2026-02-23 21:11:02 +0100

    fetch data from the backend

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -4,6 +4,14 @@ import checkmark from '../assets/images/icons/checkmark.png';
 import { products } from '../../starting-code/ecommerce-project-main/data/products';
 
 export function HomePage() {
+	fetch('http://localhost:3000/api/products')
+		.then((response) => {
+			response.json();
+		})
+		.then((data) => {
+			console.log(data);
+		});
+
 	return (
 		<>
 			<title>Ecommerce Project</title>
