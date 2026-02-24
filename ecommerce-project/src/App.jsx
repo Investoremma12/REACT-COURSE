d47586eddca1d1b81f7764ff82e331018c175979
emmanuel <onyibeemmanuel621@gmail.com>
2026-02-24 17:56:26 +0100

    Generate  HTML for the cart item

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -12,7 +12,7 @@ import { ErrorPage } from './pages/ErrorPage';
 function App() {
 	const [cart, setCart] = useState([]);
 	useEffect(() => {
-		axios.get('/api/cart-items').then((response) => {
+		axios.get('/api/cart-items?expand=product').then((response) => {
 			setCart(response.data);
 		});
 	}, []);
